ce4428c341be8ff6864abeb7dbc295a9319daf0d
白湯 <41261915+sayuprc@users.noreply.github.com>
2022-05-29 10:59:15 +0900

    feat: 文字列長の検証 (#5)

---
+++ src/Rules/SameLengthRule.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Validation\Rules;
+
+class SameLengthRule implements RuleInterface
+{
+    /**
+     * 文字列長が指定と同じ
+     *
+     * @param mixed $value
+     * @param mixed $parameters
+     *
+     * @return bool
+     */
+    public function validate(mixed $value, mixed $parameters): bool
+    {
+        if (! is_string($value)) {
+            return false;
+        } elseif (is_string($value) && mb_strlen($value) !== (int) $parameters) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ tests/Rules/MinLengthRuleTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Rules;
+
+use PHPUnit\Framework\TestCase;
+use Validation\Rules\MinLengthRule;
+
+class MinLengthRuleTest extends TestCase
+{
+    private MinLengthRule $rule;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->rule = new MinLengthRule();
+    }
+
+    /**
+     * 文字列長が指定以上の時、検証成功
+     */
+    public function testValidationSucceeded()
+    {
+        $this->assertTrue($this->rule->validate('s', '1'));
+
+        $this->assertTrue($this->rule->validate('st', '1'));
+    }
+
+    /**
+     * 文字列長が指定より小さい時、検証失敗
+     */
+    public function testValidationFailed()
+    {
+        $this->assertFalse($this->rule->validate('', '1'));
+
+        $this->assertFalse($this->rule->validate('s', '2'));
+    }
+
+    /**
+     * 値がNULLの時、検証失敗
+     */
+    public function testValidationFailedWithNull()
+    {
+        $this->assertFalse($this->rule->validate(null, '4'));
+    }
+}
